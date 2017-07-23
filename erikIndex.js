2a7d6943e3f32038a2c474bcd8c119d4bd0e422b
Erik Slikkerveer <erikslikk41@gmail.com>
2017-07-22 19:27:14 -0700

    Create erikIndex.js

---
+++ erikIndex.js
@@ -0,0 +1,42 @@
+function fade() {
+    $('.media').hide();
+    $('.media').fadeIn(2000);
+}
+
+function colorChange() {
+    $('.centered').mouseenter(function() {
+        $('.centered').fadeTo('fast', .25);
+    });
+    $('.centered').mouseleave(function() {
+        $('.centered').fadeTo('fast', 1);
+    });
+}
+
+function colorChange2() {
+    $('.centered2').mouseenter(function() {
+        $('.centered2').fadeTo('fast', .25);
+    });
+    $('.centered2').mouseleave(function() {
+        $('.centered2').fadeTo('fast', 1);
+    });
+}
+
+function colorChange3() {
+    $('.centered3').mouseenter(function() {
+        $('.centered3').fadeTo('fast', .25);
+    });
+    $('.centered3').mouseleave(function() {
+        $('.centered3').fadeTo('fast', 1);
+    });
+}
+
+function closerFade() {
+    $('.closer').hide();
+    $('.closer').fadeIn(2000);
+}
+
+$(Document).ready(fade);
+$(document).ready(colorChange);
+$(document).ready(colorChange2);
+$(document).ready(colorChange3);
+$(document).ready(closerFade);
