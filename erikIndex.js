89bcb138722bd378b82445ce55b9c47a51908ffb
Erik Slikkerveer <erikslikk41@gmail.com>
2018-01-30 10:42:18 -0800

    fixed background and lag issues

--- erikIndex.js
+++ erikIndex.js
@@ -1,43 +1,34 @@
 
 function fade() {
+    // fades in all the media, ie, the icons
     $('.media').hide();
     $('.media').fadeIn(2000);
-}
 
-function colorChange() {
-    $('.centered').mouseenter(function() {
-        $('.centered').fadeTo('fast', .25);
-    });
-    $('.centered').mouseleave(function() {
-        $('.centered').fadeTo('fast', 1);
-    });
-}
-
-function colorChange2() {
-    $('.centered2').mouseenter(function() {
-        $('.centered2').fadeTo('fast', .25);
-    });
-    $('.centered2').mouseleave(function() {
-        $('.centered2').fadeTo('fast', 1);
-    });
-}
-
-function colorChange3() {
-    $('.centered3').mouseenter(function() {
-        $('.centered3').fadeTo('fast', .25);
-    });
-    $('.centered3').mouseleave(function() {
-        $('.centered3').fadeTo('fast', 1);
-    });
-}
-
-function closerFade() {
+    // fades in the closer, ie, the 'erikslikk41@gmail.com'
     $('.closer').hide();
     $('.closer').fadeIn(2000);
 }
 
-$(Document).ready(fade);
-$(document).ready(colorChange);
-$(document).ready(colorChange2);
-$(document).ready(colorChange3);
-$(document).ready(closerFade);
+function colorChange() {
+    // on mouseenter function for all the icons to fade in and out when a mouse enters/leaves
+    $('.centered').mouseenter(function() { //  enter icon 1
+        $('.centered').fadeTo(.10, .7);
+    });
+    $('.centered').mouseleave(function() { // leave icon 1
+        $('.centered').fadeTo(.10, 1);
+    });
+    $('.centered2').mouseenter(function() { // enter icon 2
+        $('.centered2').fadeTo(.1, .7);
+    });
+    $('.centered2').mouseleave(function() { // leave icon 2
+        $('.centered2').fadeTo(.1, 1);
+    });
+    $('.centered3').mouseenter(function() { // enter icon 3
+        $('.centered3').fadeTo(.1, .7);
+    });
+    $('.centered3').mouseleave(function() { // leave icon 3
+        $('.centered3').fadeTo(.1, 1);
+    });
+}
+
+$(document).ready(fade, colorChange()); // function call 
